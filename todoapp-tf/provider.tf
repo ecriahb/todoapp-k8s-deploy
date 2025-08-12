a61e00774015c294f5b54ae48eb957c14f144fcb
ecriahb <ecriahb@gmail.com>
2025-08-12 15:21:58 +0530

    added tf and yaml

--- todoapp-tf/provider.tf
+++ todoapp-tf/provider.tf
@@ -1,6 +1,14 @@
 // providers.tf
 terraform {
   required_version = ">= 1.1.0"
+
+  backend "azurerm" {
+    resource_group_name   = "todoapp-tf"
+    storage_account_name  = "todoapptf"   # must be globally unique
+    container_name        = "tfstate"
+    key                   = "todoapp-demo.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
