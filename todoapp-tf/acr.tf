cee08c3b32a4c089cf207285e38f118e1348d9cb
ecriahb <ecriahb@gmail.com>
2025-08-14 10:40:19 +0530

    updated terrafrom

--- todoapp-tf/acr.tf
+++ todoapp-tf/acr.tf
@@ -9,11 +9,11 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = false
 }
 
-# -------------------------------
-# Allow Web App to pull from ACR
-# -------------------------------
-resource "azurerm_role_assignment" "webapp_acr_pull" {
-  scope                = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_linux_web_app.webapp.identity[0].principal_id
-}
+# # -------------------------------
+# # Allow Web App to pull from ACR
+# # -------------------------------
+# resource "azurerm_role_assignment" "webapp_acr_pull" {
+#   scope                = azurerm_container_registry.acr.id
+#   role_definition_name = "AcrPull"
+#   principal_id         = azurerm_linux_web_app.webapp.identity[0].principal_id
+# }
