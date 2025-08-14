51faa7ccf0409329d3d36e6238978a5ab40f035a
ecriahb <ecriahb@gmail.com>
2025-08-14 14:08:51 +0530

    updated add task

--- todoapp-tf/front-cdn.tf
+++ todoapp-tf/front-cdn.tf
@@ -2,19 +2,18 @@
 # Locals for Front Door
 # -------------------------------
 locals {
-  fd_profile_name           = "todoapp-fd-profile"
-  fd_endpoint_name          = "todoapp-fd-endpoint"
-  fd_origin_group_webapp    = "todoapp-fd-og-webapp"
-  fd_origin_group_appgw     = "todoapp-fd-og-appgw"
-  fd_origin_webapp          = "todoapp-fd-origin-webapp"
-  fd_origin_appgw           = "todoapp-fd-origin-appgw"
-  fd_route_name             = "todoapp-fd-route"
+  fd_profile_name      = "todoapp-fd-profile"
+  fd_endpoint_name     = "todoapp-fd-endpoint"
+  fd_origin_group_name = "todoapp-fd-origin-group"
+  fd_origin_webapp     = "todoapp-webapp-origin"
+  fd_route_name        = "todoapp-fd-route"
 }
 
 # -------------------------------
 # Front Door Profile
 # -------------------------------
 resource "azurerm_cdn_frontdoor_profile" "fd_profile" {
+depends_on = [azurerm_linux_web_app.webapp]
   name                = local.fd_profile_name
   resource_group_name = azurerm_resource_group.rg.name
   sku_name            = "Standard_AzureFrontDoor"
@@ -24,16 +23,16 @@ resource "azurerm_cdn_frontdoor_profile" "fd_profile" {
 # Front Door Endpoint
 # -------------------------------
 resource "azurerm_cdn_frontdoor_endpoint" "fd_endpoint" {
+
   name                     = local.fd_endpoint_name
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd_profile.id
 }
 
 # -------------------------------
-# Origin Groups
+# Front Door Origin Group
 # -------------------------------
-# Web App Origin Group
 resource "azurerm_cdn_frontdoor_origin_group" "fd_origin_group_webapp" {
-  name                     = local.fd_origin_group_webapp
+  name                     = local.fd_origin_group_name
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd_profile.id
   session_affinity_enabled = false
 
@@ -50,60 +49,28 @@ resource "azurerm_cdn_frontdoor_origin_group" "fd_origin_group_webapp" {
   }
 }
 
-# App Gateway Origin Group
-resource "azurerm_cdn_frontdoor_origin_group" "fd_origin_group_appgw" {
-  name                     = local.fd_origin_group_appgw
-  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd_profile.id
-  session_affinity_enabled = false
-
-  load_balancing {
-    sample_size                 = 4
-    successful_samples_required = 3
-  }
-
-  health_probe {
-    path                = "/"
-    request_type        = "HEAD"
-    protocol            = "Http"
-    interval_in_seconds = 30
-  }
-}
-
 # -------------------------------
-# Origins
+# Front Door Origin pointing to Web App
 # -------------------------------
-# Web App Origin
 resource "azurerm_cdn_frontdoor_origin" "fd_origin_webapp" {
+    depends_on = [azurerm_linux_web_app.webapp]
   name                          = local.fd_origin_webapp
   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.fd_origin_group_webapp.id
 
-  host_name          = azurerm_linux_web_app.webapp.default_site_hostname
-  http_port          = 80
-  https_port         = 443
-  origin_host_header = azurerm_linux_web_app.webapp.default_site_hostname
+  host_name                      = "${azurerm_linux_web_app.webapp.name}.azurewebsites.net"
+  http_port                      = 80
+  https_port                     = 443
+  origin_host_header             = "${azurerm_linux_web_app.webapp.name}.azurewebsites.net"
   certificate_name_check_enabled = false
-}
-
-# App Gateway Origin (internal)
-resource "azurerm_cdn_frontdoor_origin" "fd_origin_appgw" {
-  name                          = local.fd_origin_appgw
-  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.fd_origin_group_appgw.id
-
-  host_name          = azurerm_application_gateway.appgateway.frontend_ip_configuration[0].private_ip_address
-  http_port          = 80
-  https_port         = 443
-  origin_host_header = azurerm_application_gateway.appgateway.frontend_ip_configuration[0].private_ip_address
-  certificate_name_check_enabled = false
-
-  depends_on = [
-    azurerm_application_gateway.appgateway
-  ]
+  priority                       = 1
+  weight                         = 100
 }
 
 # -------------------------------
-# Routing Rule
+# Front Door Route
 # -------------------------------
-resource "azurerm_cdn_frontdoor_route" "fd_route" {
+resource "azurerm_cdn_frontdoor_route" "fd_route_webapp" {
+    depends_on = [azurerm_linux_web_app.webapp]
   name                          = local.fd_route_name
   cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.fd_endpoint.id
   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.fd_origin_group_webapp.id
@@ -115,3 +82,9 @@ resource "azurerm_cdn_frontdoor_route" "fd_route" {
   link_to_default_domain = true
   https_redirect_enabled = false
 }
+
+# -------------------------------
+# Dependencies
+# -------------------------------
+# Ensure Front Door is deployed after Web App
+
