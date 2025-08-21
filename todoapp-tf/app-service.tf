4eef83739a27dc9e688f679fa1cd7440c40bec09
ecriahb <ecriahb@gmail.com>
2025-08-21 15:53:15 +0530

    code updated

--- todoapp-tf/app-service.tf
+++ todoapp-tf/app-service.tf
@@ -6,7 +6,7 @@ resource "azurerm_service_plan" "asp" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  sku_name = "S1"
+  sku_name = "B1"
   os_type  = "Linux"
 }
 
