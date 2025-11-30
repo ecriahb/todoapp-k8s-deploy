73a3c416c95901f1741f8e83c4124b7169d25c7a
ecriahb <125296586+ecriahb@users.noreply.github.com>
2025-11-30 14:14:44 +0530

    Add SQL Server and Database resources in Terraform

---
+++ todoapp-tf/sql.tf
@@ -0,0 +1,22 @@
+# --------------------------
+# SQL Server
+# --------------------------
+resource "azurerm_mssql_server" "sqlserver" {
+  name                         = "todo-sql-server-001"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+
+  administrator_login          = "sqladminuser"
+  administrator_login_password = "StrongP@ssword123!"  # बदल लें
+}
+
+# --------------------------
+# SQL Database (todo)
+# --------------------------
+resource "azurerm_mssql_database" "todo_db" {
+  name                = "todo"
+  server_id           = azurerm_mssql_server.sqlserver.id
+  sku_name            = "S0"         # Basic/S0/S1 आदि चुन सकते हैं
+  max_size_gb         = 5
+}
