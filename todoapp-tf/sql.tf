77c2679c7e8e6f45aeb79bae2dc23641e28c331a
ecriahb <125296586+ecriahb@users.noreply.github.com>
2025-11-30 15:36:36 +0530

    Update sql.tf

--- todoapp-tf/sql.tf
+++ todoapp-tf/sql.tf
@@ -1,7 +1,7 @@
 resource "azurerm_mssql_server" "sqlserver" {
   name                         = "todo-sql-server-007"
-  resource_group_name          = "todoapp-rg"
-  location                     = "eastus2"  # Change from current restricted region
+  resource_group_name = azurerm_resource_group.rg.name
+ location            = azurerm_resource_group.rg.location
   version                      = "12.0"
   administrator_login          = "sqladminuser"
   administrator_login_password = "abcd1234"
