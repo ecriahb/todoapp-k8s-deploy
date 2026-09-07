09a47df3bc9420bbd5b34ebe0a6f9882ae61f879
ecriahb <125296586+ecriahb@users.noreply.github.com>
2026-09-07 23:17:31 +0530

    Rename SQL resources for Agentic DevOps lab

--- todoapp-tf/sql.tf
+++ todoapp-tf/sql.tf
@@ -1,19 +1,18 @@
 resource "azurerm_mssql_server" "sqlserver" {
-  name                         = "todo-sql-server-007"
-  resource_group_name = azurerm_resource_group.rg.name
- location            = azurerm_resource_group.rg.location
+  name                         = "agentic-devops-sql"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
   administrator_login          = "sqladminuser"
   administrator_login_password = "abcd@@@!!!1234"
 }
 
-
 # --------------------------
-# SQL Database (todo)
+# SQL Database
 # --------------------------
 resource "azurerm_mssql_database" "todo_db" {
-  name                = "todo"
-  server_id           = azurerm_mssql_server.sqlserver.id
-  sku_name            = "S0"         # Basic/S0/S1 आदि चुन सकते हैं
-  max_size_gb         = 5
+  name         = "todoapp"
+  server_id    = azurerm_mssql_server.sqlserver.id
+  sku_name     = "S0"
+  max_size_gb  = 5
 }
