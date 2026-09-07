21bcf92b50b478baa76b6bef1528e980a1a0b74f
ecriahb <125296586+ecriahb@users.noreply.github.com>
2026-09-07 23:17:27 +0530

    Rename VNet for Agentic DevOps lab

--- todoapp-tf/network.tf
+++ todoapp-tf/network.tf
@@ -1,6 +1,6 @@
 # Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "todoapp-vnet"
+  name                = "agentic-devops-vnet"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
@@ -13,5 +13,3 @@ resource "azurerm_subnet" "aks_subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
-
-
