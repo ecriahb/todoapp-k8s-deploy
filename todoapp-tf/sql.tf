a4457b1906afe9a011cb4555b186b61da31dd433
Brijesh Chauhan <125296586+ecriahb@users.noreply.github.com>
2026-09-10 21:46:58 +0530

    Update sql.tf

--- todoapp-tf/sql.tf
+++ todoapp-tf/sql.tf
@@ -1,18 +1,19 @@
-resource "azurerm_mssql_server" "sqlserver" {
-  name                         = "agentic-devops-sql"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  version                      = "12.0"
-  administrator_login          = "sqladminuser"
-  administrator_login_password = "abcd@@@!!!1234"
-}
+# resource "azurerm_mssql_server" "sqlserver" {
+#   name                         = "agentic-devops-sql"
+#   resource_group_name          = azurerm_resource_group.rg.name
+#   location                     = azurerm_resource_group.rg.location
+#   version                      = "12.0"
+#   administrator_login          = "sqladminuser"
+#   administrator_login_password = "abcd@@@!!!1234"
+# }
 
 # --------------------------
 # SQL Database
 # --------------------------
-resource "azurerm_mssql_database" "todo_db" {
-  name         = "agentsql"
-  server_id    = azurerm_mssql_server.sqlserver.id
-  sku_name     = "S0"
-  max_size_gb  = 5
-}
+
+# resource "azurerm_mssql_database" "todo_db" {
+#   name        = "agentsql"
+#   server_id   = azurerm_mssql_server.sqlserver.id
+#   sku_name    = "S0"
+#   max_size_gb = 5
+# }
