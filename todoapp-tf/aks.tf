410a8403e9d27d9a5213ba68368c43f9089addd6
Brijesh Chauhan <125296586+ecriahb@users.noreply.github.com>
2026-09-12 11:37:30 +0530

    Update aks.tf

--- todoapp-tf/aks.tf
+++ todoapp-tf/aks.tf
@@ -10,7 +10,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name           = "systempool"
     node_count     = 1
-    vm_size        = "Standard_D2as_v5"
+    vm_size        = "Standard_D4as_v7"
     vnet_subnet_id = azurerm_subnet.aks_subnet.id
     max_pods       = 30
   }
