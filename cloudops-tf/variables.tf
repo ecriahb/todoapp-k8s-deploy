385dd04273ee0f00fcf9fb83d25e44beeee37b64
Brijesh Chauhan <125296586+ecriahb@users.noreply.github.com>
2026-09-13 11:56:02 +0530

    cleanup: remove unused SQL region variable

--- cloudops-tf/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "sql_location" {
-  description = "Azure region used for the Azure SQL logical server. Keep this independent from the AKS/resource-group region because SQL provisioning can be region restricted."
-  type        = string
-  default     = "Central US"
-}
